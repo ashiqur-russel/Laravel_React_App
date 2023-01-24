13028d6affd94f550cd31ddb8d0196a93e58ba9f
ashiqurras <ashiqurrussel2@gmail.com>
2023-01-24 01:09:44 +0100

    Signup page design

--- client/src/Pages/Register/Register.js
+++ client/src/Pages/Register/Register.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>This is register</div>;
+  const registerHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={registerHandler}>
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Repeat Password" />
+          <button className="btn btn-block">Register</button>
+          <p className="message">
+            Already registered? <Link to="/login">Sign In</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
